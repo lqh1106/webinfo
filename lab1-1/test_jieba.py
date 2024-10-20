825195b80f791f700677012e8321958b466acdf9
lqh1106 <lqh031106@mail.ustc.edu.cn>
2024-10-20 21:33:47 +0800

    fixed some bugs

--- lab1-1/test_jieba.py
+++ lab1-1/test_jieba.py
@@ -8,16 +8,17 @@ def save_data(data):
     file_path = 'webinfo/lab1-1/dataset/selected_book_top_1200_data_tag_jieba.csv'
     df.to_csv(file_path, index=False, encoding='utf-8')
 
+
 # 读取CSV文件
 file_path = 'webinfo/lab1-1/dataset/selected_book_top_1200_data_tag.csv'  # 替换为你的CSV文件路径
 oringal_data = pd.read_csv(file_path)
 data = {"Book": [], "Tags": []}
 for book, booktags in zip(oringal_data['Book'], oringal_data['Tags']):
-    booktags= booktags.strip("{}")
+    booktags = booktags.strip("{}")
     booktags_list = [item.strip().strip("'") for item in booktags.split(",")]
     words = []
     for tag in booktags_list:
-        words.append(jieba.lcut(tag))
+        words = words+jieba.lcut(tag)
     data['Book'].append(book)
     data['Tags'].append(words)
 save_data(data)
